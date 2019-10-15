eee9e7c72a9f44453ae9087dd7b620c29d8d93ee
João Gouveia <a13734@ebsaas.com>
2019-10-15 17:28:10 +0100

    Add media

--- Aula 4/src/pt/uma/ctesp/aula4/Exercises.java
+++ Aula 4/src/pt/uma/ctesp/aula4/Exercises.java
@@ -11,11 +11,17 @@ package pt.uma.ctesp.aula4;
  */
 public class Exercises {
     public static void printGrades(int[] numbers, String [] acronyms, int[][] grades){
+        float media = 0;
+        float soma = 0;
         for(int i = 0; i < numbers.length; i++){
+            soma = 0;
             System.out.println("As notas do aluno-> " + numbers[i] +  " sao:");
             for(int x = 0; x < grades[x].length; x++){
+                soma += grades[i][x];
                 System.out.println(acronyms[x] + "-> " + grades[i][x]);
             }
+            media = soma / 3;
+            System.out.println("Média: " + Math.round(media * 100.0) / 100.0);
         }
     }
 }
