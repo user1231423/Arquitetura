ce463375fba57f0629b4e55aa33378147d27ff9c
João Gouveia <a13734@ebsaas.com>
2019-10-15 17:31:21 +0100

    Add space

--- Aula 4/src/pt/uma/ctesp/aula4/Exercises.java
+++ Aula 4/src/pt/uma/ctesp/aula4/Exercises.java
@@ -10,18 +10,20 @@ package pt.uma.ctesp.aula4;
  * @author joaogouveia
  */
 public class Exercises {
-    public static void printGrades(int[] numbers, String [] acronyms, int[][] grades){
+
+    public static void printGrades(int[] numbers, String[] acronyms, int[][] grades) {
         float media = 0;
         float soma = 0;
-        for(int i = 0; i < numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             soma = 0;
-            System.out.println("As notas do aluno-> " + numbers[i] +  " sao:");
-            for(int x = 0; x < grades[x].length; x++){
+            System.out.println("As notas do aluno-> " + numbers[i] + " sao:");
+            for (int x = 0; x < grades[x].length; x++) {
                 soma += grades[i][x];
                 System.out.println(acronyms[x] + "-> " + grades[i][x]);
             }
             media = soma / 3;
             System.out.println("Média: " + Math.round(media * 100.0) / 100.0);
+            System.out.println(" ");
         }
     }
 }
